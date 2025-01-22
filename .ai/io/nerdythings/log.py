a82c65a1d5d8ecf9b5d7461bcbd54eea54bfadfa
abdulwaheedsoudagar <soudagarabdulwaheed@gmail.com>
2025-01-22 20:44:28 +0530

    added pr changs

---
+++ .ai/io/nerdythings/log.py
@@ -0,0 +1,21 @@
+class Log:
+    # ANSI escape codes for some colors
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    RESET = '\033[0m'
+
+    @staticmethod
+    def print_red(*args):
+        text = ' '.join(str(arg) for arg in args)
+        print(f"{Log.RED}{text}{Log.RESET}")
+
+    @staticmethod
+    def print_green(*args):
+        text = ' '.join(str(arg) for arg in args)
+        print(f"{Log.GREEN}{text}{Log.RESET}")
+
+    @staticmethod
+    def print_yellow(*args):
+        text = ' '.join(str(arg) for arg in args)
+        print(f"{Log.YELLOW}{text}{Log.RESET}")
