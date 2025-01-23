99692a0818b7f82bef4e15fadc2639790ef07bed
abdulwaheedsoudagar <soudagarabdulwaheed@gmail.com>
2025-01-23 19:11:34 +0530

    dslkfjsdlfksjdf

--- .ai/io/nerdythings/ai/chat_gpt.py
+++ .ai/io/nerdythings/ai/chat_gpt.py
@@ -25,5 +25,5 @@ class ChatGPT(AiBot):
         # for chunk in stream:
         #     if chunk.choices[0].delta.content:
         #         content.append(chunk.choices[0].delta.content)
-        return " ".join(content)
+        return content
     
